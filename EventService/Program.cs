3b3520e585a6e2fb39fdf51f6a8e1bf58c5c7249
omarade <3omar.a.de@gmail.com>
2024-01-17 11:56:02 +0100

    fixed venue update conusmer, use in memory db for test

--- EventService/Program.cs
+++ EventService/Program.cs
@@ -24,10 +24,15 @@ if (builder.Environment.IsProduction())
     connectionString = Environment.GetEnvironmentVariable("EVENT_DB_CONNECTION_STRING");
     Console.WriteLine(connectionString);
 
-    Console.WriteLine("----> Using SqlServer Db");
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(connectionString)
-    ); 
+    // Console.WriteLine("----> Using SqlServer Db");
+    // builder.Services.AddDbContext<AppDbContext>(options =>
+    //     options.UseSqlServer(connectionString)
+    // );
+
+     Console.WriteLine("----> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => 
+        opt.UseInMemoryDatabase("InMem")
+    );
 }
 else 
 {
